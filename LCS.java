0326f8db8e8a309ca172cd7b95c3257bdbba2cbf
lnss7 <novellysimaolucas@gmail.com>
2025-10-15 11:23:54 -0300

    tá lá!

--- LCS.java
+++ LCS.java
@@ -1,3 +1,78 @@
 public class LCS {
-    
+
+    public static long iterations = 0;
+    public static long lastTimeNs = 0;
+
+    public static long iterationsDynamic = 0;
+    public static long lastTimeNsDynamic = 0;
+
+    public static String forcaBruta(String a, String b) {
+        iterations = 0;
+        long start = System.nanoTime();
+        String seq = lcsRec(a, b, a.length() - 1, b.length() - 1);
+        lastTimeNs = System.nanoTime() - start;
+        return seq;
+    }
+
+    private static String lcsRec(String a, String b, int i, int j) {
+        iterations++;
+        if (i < 0 || j < 0)
+            return "";
+
+        if (a.charAt(i) == b.charAt(j)) {
+            return lcsRec(a, b, i - 1, j - 1) + a.charAt(i);
+        } else {
+            String s1 = lcsRec(a, b, i - 1, j);
+            String s2 = lcsRec(a, b, i, j - 1);
+            if (s1.length() >= s2.length()) {
+                return s1;
+            } else {
+                return s2;
+            }
+        }
+
+    }
+
+    public static String dynamic(String a, String b) {
+        iterationsDynamic = 0;
+        long start = System.nanoTime();
+        String seq = lcsDynamic(a, b);
+        lastTimeNsDynamic = System.nanoTime() - start;
+        return seq;
+    }
+
+    private static String lcsDynamic(String a, String b) {
+        int m = a.length();
+        int n = b.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                iterationsDynamic++; // conta operações do DP
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        StringBuilder lcs = new StringBuilder();
+        int i = m, j = n;
+        while (i > 0 && j > 0) {
+            iterationsDynamic++; // conta passos de reconstrucao
+            if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                lcs.append(a.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        lcs.reverse();
+        return lcs.toString();
+    }
 }
---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // Exemplo simples usando LCS recursivo por força bruta
+        String a = "AGGTXAB"; // exemplo
+        String b = "AXTXABY"; // exemplo
+
+        System.out.println("Entrada A: " + a);
+        System.out.println("Entrada B: " + b);
+
+        String seq = LCS.forcaBruta(a, b);
+
+        System.out.println("LCS (força bruta): " + seq);
+        System.out.println("Iterações (chamadas recursivas): " + LCS.iterations);
+        System.out.printf("Tempo decorrido: %.3f ms\n", LCS.lastTimeNs / 1_000_000.0);
+
+        // Exemplo simples usando LCS com programação dinâmica
+        String seqDyn = LCS.dynamic(a, b);
+        System.out.println("LCS (programação dinâmica): " + seqDyn);
+        System.out.println("Iterações (programação dinâmica): " + LCS.iterationsDynamic);
+        System.out.printf("Tempo decorrido: %.3f ms\n", LCS.lastTimeNsDynamic / 1_000_000.0);
+
+    }
+}
