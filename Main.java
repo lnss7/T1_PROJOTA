cbe5e1f65f3d5262fb5300bc605c57169cd42473
Leo <leofwingert@gmail.com>
2025-10-15 11:55:45 -0300

    Remove commented-out code for brute force LCS implementation and add output for LCS length

--- Main.java
+++ Main.java
@@ -7,15 +7,16 @@ public class Main {
         System.out.println("Entrada A: " + a);
         System.out.println("Entrada B: " + b);
 
-        String seq = LCS.forcaBruta(a, b);
+        //String seq = LCS.forcaBruta(a, b);
 
-        System.out.println("LCS (força bruta): " + seq);
-        System.out.println("Iterações (chamadas recursivas): " + LCS.iterations);
-        System.out.printf("Tempo decorrido: %.3f ms\n", LCS.lastTimeNs / 1_000_000.0);
+        //System.out.println("LCS (força bruta): " + seq);
+        //System.out.println("Iterações (chamadas recursivas): " + LCS.iterations);
+        //System.out.printf("Tempo decorrido: %.3f ms\n", LCS.lastTimeNs / 1_000_000.0);
 
         // Exemplo simples usando LCS com programação dinâmica
         String seqDyn = LCS.dynamic(a, b);
         System.out.println("LCS (programação dinâmica): " + seqDyn);
+        System.out.println("Tamanho da LCS: " + seqDyn.length());
         System.out.println("Iterações (programação dinâmica): " + LCS.iterationsDynamic);
         System.out.printf("Tempo decorrido: %.3f ms\n", LCS.lastTimeNsDynamic / 1_000_000.0);
 
